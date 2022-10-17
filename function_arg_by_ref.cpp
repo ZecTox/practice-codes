4a0050ee209d25ab656c74cd3d54231b05cd72d9
Tejas Kedare <82746525+ZecTox@users.noreply.github.com>
2022-10-17 22:54:15 +0530

    Update function_arg_by_ref.cpp

--- function_arg_by_ref.cpp
+++ function_arg_by_ref.cpp
@@ -1,16 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void addone(int *n) {
-    (*n)++;
+typedef struct {
+  char * name;
+  int age;
+} person;
+
+void birthday(person * p);
+
+void birthday(person * p){
+    p->age++;
 }
 
-int main(){
-    
-    int n = 1;
-    cout << n <<endl;
-    addone(&n);
-    cout << n << endl;
-    
-    return 0;
+int main() {
+  person john;
+  john.name = "John";
+  john.age = 27;
+
+  printf("%s is %d years old.\n", john.name, john.age);
+  birthday(&john);
+  printf("Happy birthday! %s is now %d years old.\n", john.name, john.age);
+
+  return 0;
 }
