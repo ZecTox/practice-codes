63ffb0703a26b1a133ab2cf219fa013677b506bd
Tejas Kedare <82746525+ZecTox@users.noreply.github.com>
2022-11-08 20:37:37 +0530

    Update pair sum problem optimized answer.cpp

--- pair sum problem optimized answer.cpp
+++ pair sum problem optimized answer.cpp
@@ -1,15 +1,21 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 bool pairsum(int arr[], int n, int k){
     
     int low = 0;
     int high = n-1;
     
     while (low<high){
-        if(arr[low])
+        if(arr[low] + arr[high] == k){
+            cout << low << " " << high << endl;
+            return true;
+        }
+        else if(arr[low] + arr[high] > k){
+            high--;
+        }
+        else{
+            low++;
+        }
     }
-    
+    return false;
 }
 
 int main(){
