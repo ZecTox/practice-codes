5265ed1944403382b2aab9c8b4bbeb0dbbc775f6
Tejas Kedare <82746525+ZecTox@users.noreply.github.com>
2022-11-11 23:31:56 +0530

    Create how to print multiple things with string.cpp

---
+++ how to print multiple things with string.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   string str;
+   string str1(5, 'n');
+   cout << str1 << endl;
+   
+    return 0;
+}
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string str;
+    getline(cin, str);
+    cout << str << endl;
+   
+    return 0;
+}
+  
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  
+  
