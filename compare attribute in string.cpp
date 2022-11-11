7472446d2a96b1a52435ca407accd473c880ac04
Tejas Kedare <82746525+ZecTox@users.noreply.github.com>
2022-11-12 00:11:05 +0530

    Create compare attribute in string.cpp

---
+++ compare attribute in string.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+   string s1 = "abc";
+   string s2 = "abc";
+   
+   if(s1.compare(s2) == 0){
+       cout << "strings are equal" << endl;
+   }
+   cout << s2.compare(s1) << endl;
+   
+   
+    return 0;
+}
