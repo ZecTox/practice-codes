5b4fc4edca16ae402024d92279c96e76b2a82d3b
Tejas Kedare <82746525+ZecTox@users.noreply.github.com>
2022-11-12 00:50:50 +0530

    Create sorting attribute in string.cpp

---
+++ sorting attribute in string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    string s1 = "skljdfakljsgsdakjfhhjkds";
+    
+    sort(s1.begin(), s1.end());
+    
+    cout << s1 << endl;
+    
+    return 0;
+}
