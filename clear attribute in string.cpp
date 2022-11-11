c7c032e8346e68f41013c25baf5f600d323fa70e
Tejas Kedare <82746525+ZecTox@users.noreply.github.com>
2022-11-11 23:43:56 +0530

    Create clear attribute in string.cpp

---
+++ clear attribute in string.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    string abc = " dhjkfgbkjhsdg vhjf ksdfksd hf gkjhsfd";
+    abc.clear();
+    cout << abc;
+    
+    return 0;
+}
