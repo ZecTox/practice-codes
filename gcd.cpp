ea45b0d357ba40bd30f99f84e8d2412ab20c14c7
Tejas Kedare <82746525+ZecTox@users.noreply.github.com>
2023-02-21 21:43:59 +0530

    Create gcd.cpp

---
+++ gcd.cpp
@@ -0,0 +1,11 @@
+ #include<bits/stdc++.h>
+ #include <numeric>
+ using namespace std;
+
+ int main(){
+    int a,b;
+    cin >> a >> b;
+    int c = gcd(a, b);
+    cout << c << endl;
+return 0; 
+ }
